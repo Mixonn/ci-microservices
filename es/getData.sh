afe76998694919fbc22a221ec4bd20eb0330cc6c
Osipiuk, Bartosz <osipiuk.bartosz@gmail.com>
2020-02-04 21:16:51 +0100

    Update docker-compose

--- es/getData.sh
+++ es/getData.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
+#https://www.elastic.co/guide/en/kibana/5.5/tutorial-load-dataset.html
 
-There is a tutorial to load data
-https://www.elastic.co/guide/en/kibana/5.5/tutorial-load-dataset.html
+wget https://download.elastic.co/demos/kibana/gettingstarted/shakespeare.json
--- run-bg.sh
+++ run-bg.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 gradle clean build
-docker-compose up -d consul
+docker-compose up -d
 while ! curl -Ssf localhost:8500; do
     sleep 2
 done
 sleep 1
 ./ci-core/consul/consul-publish.sh
-docker-compose up -d ci-core
