1d67489e4edab26d2d67ab4cedb00415d2ab79cf
Osipiuk, Bartosz <bartosz.osipiuk@impaqgroup.com>
2020-02-05 12:23:56 +0100

    WIP elasticsearch

--- ci-core/src/main/java/com/cimicroservices/core/RestProperties.java
+++ ci-core/src/main/java/com/cimicroservices/core/RestProperties.java
@@ -1,6 +1,5 @@
 package com.cimicroservices.core;
 
-import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +10,5 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @Data
 public class RestProperties {
-  @NotNull private String prop;
+  private String prop;
 }
